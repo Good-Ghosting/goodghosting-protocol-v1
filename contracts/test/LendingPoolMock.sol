31738f22155366b4795f0eeb08864e5ae2833dcd
Axe <ngsmo@iscte-iul.pt>
2020-03-07 17:57:43 +0000

    calculate fake interest

--- contracts/test/LendingPoolMock.sol
+++ contracts/test/LendingPoolMock.sol
@@ -30,6 +30,7 @@ contract LendingPoolAddressesProviderMock
     function getLendingPoolConfigurator() public view returns (address) {
 
     }
+
     function setLendingPoolConfiguratorImpl(address _configurator) public {
 
     }
@@ -37,6 +38,7 @@ contract LendingPoolAddressesProviderMock
     function getLendingPoolDataProvider() public view returns (address) {
 
     }
+
     function setLendingPoolDataProviderImpl(address _provider) public {
 
     }
@@ -51,14 +53,15 @@ contract LendingPoolAddressesProviderMock
     function getTokenDistributor() public view returns (address) {
 
     }
+
     function setTokenDistributor(address _tokenDistributor) public {
 
     }
 
-
     function getFeeProvider() public view returns (address) {
 
     }
+
     function setFeeProviderImpl(address _feeProvider) public {
 
     }
@@ -66,6 +69,7 @@ contract LendingPoolAddressesProviderMock
     function getLendingPoolLiquidationManager() public view returns (address) {
 
     }
+
     function setLendingPoolLiquidationManager(address _manager) public {
 
     }
@@ -73,6 +77,7 @@ contract LendingPoolAddressesProviderMock
     function getLendingPoolManager() public view returns (address) {
 
     }
+
     function setLendingPoolManager(address _lendingPoolManager) public {
 
     }
@@ -80,6 +85,7 @@ contract LendingPoolAddressesProviderMock
     function getPriceOracle() public view returns (address) {
 
     }
+
     function setPriceOracle(address _priceOracle) public {
 
     }
@@ -87,6 +93,7 @@ contract LendingPoolAddressesProviderMock
     function getLendingRateOracle() public view returns (address) {
 
     }
+
     function setLendingRateOracle(address _lendingRateOracle) public {
 
     }
@@ -110,8 +117,9 @@ contract LendingPoolAddressesProviderMock
     }
 
     //We need to bootstrap the pool with liquidity to pay interest
-    function addLiquidity(address _reserve, address _addr, uint256 _amount) public {
+    function addLiquidity(address _reserve, address _bank, address _addr, uint256 _amount) public {
         IERC20 reserve = IERC20(_reserve);
         reserve.transferFrom(_addr, address(this), _amount);
+        _mint(_bank, _amount);
     }
 }
