38be97c0d02353425e615eafbfca850c410531d6
fodisi <francis2301@gmail.com>
2020-08-23 17:41:43 -0400

    Updates unit test coverage info

--- .solcover.js
+++ .solcover.js
@@ -1,4 +1,4 @@
 module.exports = {
     copyPackages: ['@openzeppelin/contracts'],
-    skipFiles: ['Migrations.sol', 'LendingPoolMock.sol', 'SimpleMintable.sol','TestBundle.sol']
+    skipFiles: ['Migrations.sol', 'test/LendingPoolMock.sol', 'test/SimpleMintable.sol','test/TestBundle.sol']
 };
