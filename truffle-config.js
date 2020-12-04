2aa771088f82cc3afa9935ba056e2167dbf8c346
fodisi <francis2301@gmail.com>
2020-12-04 17:49:28 -0500

    refactor: compiles contracts to ./build folder and deletes unused app folder

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
-    contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+    // contracts_build_directory: path.join(__dirname, "app/src/contracts"),
 
     plugins: [
         //"truffle-security",
