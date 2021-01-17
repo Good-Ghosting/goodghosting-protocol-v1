0441cf97d235db9b22640debf2117260391608ff
Deepcryptodive <61325205+Deepcryptodive@users.noreply.github.com>
2021-01-17 11:31:34 +0100

    Updated Aave v2 ropsten addresses & increased DAI

--- deploy.config.js
+++ deploy.config.js
@@ -2,24 +2,24 @@
  * Deployment configs for supported lending providers and networks
  *
  * Aave
- * Addresses available at https://docs.aave.com/developers/deployed-contracts/deployed-contract-instances
+ * Addresses available at https://docs.aave.com/developers/deployed-contracts (v2) & https://docs.aave.com/developers/deployed-contracts/deployed-contract-instances (v1)
  *
 */
 
 exports.providers = {
     aave: {
         kovan: {
-            lendingPoolAddressProvider: "0x652B2937Efd0B5beA1c8d54293FC1289672AFC6b",
-            dataProvider: "0x744C1aaA95232EeF8A9994C4E0b3a89659D9AB79",
+            lendingPoolAddressProvider: "0x88757f2f99175387ab4c6a4b3067c77a695b0349",
+            dataProvider: "0x3c73a5e5785cac854d468f727c606c07488a29d6",
             dai: {
                 address: "0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD",
                 decimals: 18,
             },
         },
         ropsten: {
-            lendingPoolAddressProvider: "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728",
-            dai: {
-                address: "0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108",
+            lendingPoolAddressProvider: "0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728",   /* Note: Ropsten depreciated in Aave v2 */
+            dai: {                                                                      /* Note: Ropsten depreciated in Aave v2 */
+                address: "0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108",                  /* Note: Ropsten depreciated in Aave v2 */
                 decimals: 18,
             },
         },
@@ -37,8 +37,8 @@ exports.providers = {
 exports.deployConfigs = {
     selectedProvider: "aave", // name of the selected provider. Must be defined in the object {providers} above.
     inboundCurrencySymbol: "dai", // name of the inbound currency symbol. Must be defined in the object {providers.network} above.
-    segmentCount: 6, // integer number of segments
+    segmentCount: 4, // integer number of segments
     segmentLength: 180, // in seconds
-    segmentPayment: 10, // amount of tokens - i.e. 10 equals to 10 TOKENS (DAI, ETH, etc.);
+    segmentPayment: 200, // amount of tokens - i.e. 10 equals to 10 TOKENS (DAI, ETH, etc.);
     earlyWithdrawFee: 10, // i.e. 10 equals to 10%
 };
