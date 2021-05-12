739c3ea8859faf881d0cac73f60f786e1b2fa839
fodisi <36540281+fodisi@users.noreply.github.com>
2021-05-12 06:06:07 -0400

    fix: fixes and refactors deployment script (#102)
    * fix: fixes and refactors deployment script
    
    * chore: removes unnecessary console.log()
    
    * refactor: sets default gasPrice for polygon deployments

--- deploy.config.js
+++ deploy.config.js
@@ -62,5 +62,5 @@ exports.deployConfigs = {
     segmentPayment: 10, // amount of tokens - i.e. 10 equals to 10 TOKENS (DAI, ETH, etc.);
     earlyWithdrawFee: 10, // i.e. 10 equals to 10%
     customFee: 5, // i.e. 5 equals to 5%
-    merkelroot: "0xd566243e283f1357e5e97dd0c9ab0d78177583074b440cb07815e05f615178bf" // merkel root for 1st 4 player addresses in the fork tests
+    merkleroot: "0xd566243e283f1357e5e97dd0c9ab0d78177583074b440cb07815e05f615178bf" // merkel root for 1st 4 player addresses in the fork tests
 };
