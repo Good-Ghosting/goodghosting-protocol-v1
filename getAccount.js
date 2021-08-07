9aec9fadb70b6374071bfd2f79be7946045bc8eb
fodisi <francis2301@gmail.com>
2021-08-06 20:51:11 -0400

    fix: fixes unhandled exception thrown at the end of coverage/test
    * fixes unhandled exception thrown after test coverage/unit tests due to no CELO key set

--- getAccount.js
+++ getAccount.js
@@ -1,16 +1,18 @@
-const Web3 = require('web3')
-const fs = require('fs')
-const path = require('path')
-var web3 = new Web3()
+const Web3 = require("web3");
+var web3 = new Web3();
 require("dotenv").config();
 
 function getAccount() {
     return new Promise(resolve => {
-                const key = process.env.CELO_PRIVATE_KEY
-                resolve(web3.eth.accounts.privateKeyToAccount(key.trim()))
-})
+        const key = process.env.CELO_PRIVATE_KEY;
+        if (key) {
+            resolve(web3.eth.accounts.privateKeyToAccount(key.trim()));
+        } else {
+            resolve("");
+        }
+    });
 }
 
 module.exports = {
     getAccount
-}
+};
