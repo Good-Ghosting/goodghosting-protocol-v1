1cf86a6eaac97debae0f6c6837d90eb2d19bde29
viraj124 <virajm72@gmail.com>
2021-09-06 13:37:17 +0530

    feat: add tests for registry contract

--- contracts/GoodGhostingRegistry.sol
+++ contracts/GoodGhostingRegistry.sol
@@ -8,7 +8,7 @@ contract GoodGhostingRegistry is Ownable {
     event RegistryInitialized(address[] contracts);
     event RegistryUpdated(address contracts);
 
-    address[] public contracts;
+    mapping(address => bool) public pools;
 
     function isValid(address _contract) internal {
         require(_contract != address(0), "invalid _contract address");
@@ -17,14 +17,14 @@ contract GoodGhostingRegistry is Ownable {
     constructor(address[] memory _contracts) public {
         for (uint i = 0; i < _contracts.length; i++) {
             isValid(_contracts[i]);
+            pools[_contracts[i]] = true;
         }
-        contracts = _contracts;
         emit RegistryInitialized(_contracts);
     }
 
     function addContract(address _contract) external onlyOwner {
         isValid(_contract);
-        contracts.push(_contract);
+        pools[_contract] = true;
         emit RegistryUpdated(_contract);
     }
 }
